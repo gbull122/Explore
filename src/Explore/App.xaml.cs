022179b066e36d3a7a9ddac66bd08f7a4fd524f5
gbull122 <gbull122@gmail.com>
2021-07-28 21:07:52 +0100

    Added solution and application

---
+++ src/Explore/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Explore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+
+        protected override Window CreateShell()
+        {
+            var mainWindow = Container.Resolve<MainWindow>();
+            return mainWindow;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
